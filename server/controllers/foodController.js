ccc6bea8d433db126cbe9a33df5209bd58c9d3f5
practice <nisha452457@gmail.com>
2026-07-17 21:35:28 +0530

    food module added

--- server/controllers/foodController.js
+++ server/controllers/foodController.js
@@ -56,7 +56,100 @@ const getAllFoods = async (req, res) => {
   }
 };
 
+// Update Food
+const updateFood = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const food = await Food.findByIdAndUpdate(
+      id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!food) {
+      return res.status(404).json({
+        success: false,
+        message: "Food not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Food updated successfully",
+      food,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+
+// Delete Food
+const deleteFood = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const food = await Food.findByIdAndDelete(id);
+
+    if (!food) {
+      return res.status(404).json({
+        success: false,
+        message: "Food not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Food deleted successfully",
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+
+const getFoodById = async (req, res) => {
+  try {
+    const food = await Food.findById(req.params.id);
+
+    if (!food) {
+      return res.status(404).json({
+        success: false,
+        message: "Food not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      food,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+
 module.exports = {
   addFood,
   getAllFoods,
+  getFoodById,
+  updateFood,
+  deleteFood,
 };
--- server/routes/foodRoutes.js
+++ server/routes/foodRoutes.js
@@ -4,14 +4,22 @@ const router = express.Router();
 const {
   addFood,
   getAllFoods,
+  getFoodById,
+  updateFood,
+  deleteFood,
 } = require("../controllers/foodController");
 
 const protect = require("../middleware/authMiddleware");
+
+
+router.put("/:id", protect, updateFood);
 
 // Admin - Add Food
 router.post("/", protect, addFood);
 
 // Student/Admin - Get All Foods
 router.get("/", getAllFoods);
+
+router.delete("/:id", protect, deleteFood);
 
 module.exports = router;
